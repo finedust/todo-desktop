45455c324b738e1839fdea9669e553a7d7bbc45f
Francesco <frenci98@gmail.com>
2018-02-20 19:18:38 +0100

    Changed todo.txt formatting

---
+++ README.rst
@@ -0,0 +1,13 @@
+# todo-desktop
+A simple yet powerful todo desktop application based on a slightly different format with respect to todo.txt
+
+# Task Formatting
+; (B) 2015-04-23 Task marked for later :LATER
+(A) .2017-05-12 2014-04-12 Overdue task :OVERDUE
+(C) 2012-03-03 Task with high priority (don't use higher priorities)
+.2018-02-12 2013-03-15 Job with due date
+2014-03-12 Minimal job
+2013-02-23 Job with a project +projectA
+2013-02-24 Second job of the previous task +projectA#2
+2011-03-21 Another todo with a tag :TAG
+;; comment to file
--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3.7
+#! /usr/bin/env python3.6
 
 ###
 # File: todo.py
@@ -10,15 +10,12 @@
 
 from argparse import ArgumentParser
 
-from ansicolor import color
-
 from os.path import expanduser, join as pjoin, isfile, isdir, dirname
 from os import makedirs
 
 from re import match, compile as compile_regex
 
-from time import strptime, localtime
-import time
+from datetime import datetime, timedelta
 
 import string
 
@@ -26,11 +23,12 @@ import traceback
 
 
 TODO_FILE = pjoin( expanduser("~"), "Documents", "todo.txt" )
-TODO_PATTERN = compile_regex( r"(?:(?P<later>\;\ )?(?:\((?P<priority>[A-Z])\)\ )?(?:\.(?P<due_date>\d{4}\-\d{2}\-\d{2})\ )?(?:(?P<creation_date>\d{4}\-\d{2}\-\d{2})\ )?(?P<todo>[^\&\+\n]*[^\&\+\ ])(?:\ \+(?P<project_name>[^\s\#]+)(?:\#(?P<project_seq>\d+))?)?(?P<tags>(?:\ \&[A-Z\_\d]+)*)?)|^(?P<commented>\;\;).*" )
+TODO_PATTERN = compile_regex( r"(?:(?P<later>\;\ )?(?:\((?P<priority>[A-Z])\)\ )?(?:\.(?P<due_date>\d{4}\-\d{2}\-\d{2})\ )?(?:(?P<creation_date>\d{4}\-\d{2}\-\d{2})\ )?(?P<todo>[^\:\+\n]*[^\:\+\ ])(?:\ \+(?P<project_name>[^\s\#]+)(?:\#(?P<project_seq>\d+))?)?(?P<tags>(?:\ \:[A-Z\_\d]+)*)?)|^(?P<comment>\;\;).*" )
+URGENT_TIME = timedelta(days = 7)
 
 
 class TodoTask:
-	def __init__(self, line, comment = False):
+	def __init__(self, line, comment = False, prioritize = True):
 		line = line.strip()
 		if comment or line.startswith(";; "):
 			self.later = None
@@ -40,22 +38,42 @@ class TodoTask:
 			self.todo = None
 			self.project_name = None
 			self.project_seq = None
-			self.tags = None
-			self.commented = True
+			self.tags = set()
+			self.comment = True
 			self.text = line if line.startswith(";; ") else ";; " + line
 		else:
 			tm = match(TODO_PATTERN, line)
 			if not tm: raise RuntimeError(f"Malformed task: {line}")
-			self.later = True if tm.group('later') else False
+			
+			self.tags = set([ t.strip() for t in tm.group('tags').split(':')[1:] if t ] if tm.group('tags') else [])
+			
+			self.due_date = datetime.strptime(tm.group('due_date'), "%Y-%m-%d") if tm.group('due_date') else None
+			if self.due_date and self.due_date < datetime.now(): self.tags.add("OVERDUE")
+
 			self.priority = tm.group('priority')
-			self.due_date = strptime(tm.group('due_date'), "%Y-%m-%d") if tm.group('due_date') else None
-			self.creation_date = strptime(tm.group('creation_date'), "%Y-%m-%d") if tm.group('creation_date') else None
+			if "OVERDUE" in self.tags:
+				self.priority = "A"
+			elif self.due_date and self.due_date - datetime.now() < URGENT_TIME and self.priority != "A" and self.priority != "B":
+				self.priority = "C"
+			
+			self.creation_date = datetime.strptime(tm.group('creation_date'), "%Y-%m-%d") if tm.group('creation_date') else datetime.now()
+			
 			self.todo = tm.group('todo').capitalize()
+			
 			self.project_name = tm.group('project_name')
 			self.project_seq = int(tm.group('project_seq')) if tm.group('project_seq') else 0
-			self.tags = [ t.strip() for t in tm.group('tags').split('&')[1:] ] if tm.group('tags') else None
-			self.commented = True if tm.group('commented') or self.later else False
-			self.text = line
+			
+			if tm.group('later'):
+				self.later = True
+				self.tags.add("LATER")
+			elif self.tags.intersection({"LATER", "WAITING"}):
+				self.later = True
+			else:
+				self.later = False
+			
+			self.comment = False
+			
+			self.text = make_todo(self.todo, self.due_date, self.priority, self.creation_date, self.project_name, self.project_seq, self.tags)
 
 	def __str__(self):
 		return self.text
@@ -67,51 +85,51 @@ class TodoTask:
 		return self.__hash__() == other.__hash__()
 
 	def colored(self):
-		if self.commented: return color(self.text, 'white')
+		if self.comment or self.later: return color(self.text, 'white')
 
 		ts = ""
 		if self.priority:
 			ts += color("(", 'white') 
 			if self.priority == 'A': pcolor = 'red'
-			elif self.priority == 'B': pcolor = 'yellow'
+			elif self.priority == 'B' or self.priority == 'C': pcolor = 'yellow'
 			else: pcolor = 'green'
 			ts += color(self.priority, pcolor, bold = True) 
 			ts += color(") ", 'white')
 		if self.due_date:
 			ts += color(".", 'white')
-			ts += color( str(self.due_date.tm_year) + "-" + str(self.due_date.tm_mon).zfill(2) + "-" + str(self.due_date.tm_mday).zfill(2) + " " , 'magenta', bold = True)
-		if self.creation_date:
-			ts += color( str(self.creation_date.tm_year) + "-" + str(self.creation_date.tm_mon).zfill(2) + "-" + str(self.creation_date.tm_mday).zfill(2) + " " , 'white')
+			ts += color(self.due_date.strftime("%y-%m-%d "), 'magenta', bold = True)
+		#if self.creation_date:
+		ts += color(self.creation_date.strftime("%y-%m-%d "), 'white')
 		ts += color(self.todo, bold = True)
 		if self.project_name:
 			ts += " " + color("+", 'white') + color(self.project_name, 'blue')
 			if self.project_seq: ts += color("#" + str(self.project_seq), 'cyan')
 		if self.tags:
 			for tag in self.tags:
-				ts += " " + color("&", 'white') + ( color(tag, 'red') if tag == "OVERDUE" else color(tag) )
+				ts += " " + color(":", 'white') + ( color(tag, 'red') if tag == "OVERDUE" else color(tag, 'green') )
 
 		return ts
 		
 
-	def make_todo(todo, due_date = None, priority = None, creation_date = localtime(), project_name = None, project_seq = None, tags = []):
-		string = ""
-		if "LATER" in tags: string += "; "
-		if priority and priority in string.ascii_uppercase: string += "(" + priority + ")" + " "
-		if due_date and type(due_date) == time.struct_time:
-			string += "." + str(due_date.tm_year) + "-" + str(due_date.tm_mon).zfill(2) + "-" + str(due_date.tm_mday).zfill(2) + " "
-		if creation_date and type(creation_date) == time.struct_time:
-			string += str(creation_date.tm_year) + "-" + str(creation_date.tm_mon).zfill(2) + "-" + str(creation_date.tm_mday).zfill(2) + " "
-		if not todo.strip(): raise RuntimeError( "Empty todo task" )
-		string += todo.capitalize()
-		if project_name:
-			string += " " + f"+{project_name}"
-			if project_seq: string += f"#{project_seq}"
-		if tags:
-			for t in tags:
-				t = t.strip()
-				if t:
-					string += " " + "&" + t.upper()
-		return string
+def make_todo(todo, due_date = None, priority = None, creation_date = datetime.now(), project_name = None, project_seq = None, tags = set()):
+	todo_string = ""
+	if tags.intersection({"LATER", "WAITING"}): todo_string += "; "
+	if priority and priority in string.ascii_uppercase: todo_string += "(" + priority + ")" + " "
+	if due_date and type(due_date) == datetime:
+		todo_string += due_date.strftime(".%y-%m-%d ")
+	if creation_date and type(creation_date) == datetime:
+		todo_string += creation_date.strftime("%y-%m-%d ")
+	if not todo.strip(): raise RuntimeError( "Empty todo task" )
+	todo_string += todo.capitalize()
+	if project_name:
+		todo_string += " " + f"+{project_name}"
+		if project_seq: todo_string += f"#{project_seq}"
+	if tags:
+		for t in tags:
+			t = t.strip()
+			if t:
+				todo_string += " :" + t.upper()
+	return todo_string
 
 
 def get_tasks(filepath):
@@ -130,8 +148,12 @@ def get_tasks(filepath):
 	return todos
 
 
-def list():
-	tasks = get_tasks(TODO_FILE)
+def list(projects = [], tags = []):
+	tasks = get_tasks(args.file)
+
+	if tags:
+		for task in set(tasks):
+			if len( task.tags.intersection(set(tags)) ) <= 0: tasks.discard(task)
 
 	tasks = sorted(tasks, key = lambda x: x.text)
 
@@ -140,7 +162,7 @@ def list():
 
 
 def add(*tasks):
-	todos = get_tasks(TODO_FILE)
+	todos = get_tasks(args.file)
 
 	for task in tasks:
 		try:
@@ -153,7 +175,7 @@ def add(*tasks):
 
 	todos = sorted(todos, key = lambda x: x.text)
 
-	tf = open(TODO_FILE, 'w')
+	tf = open(args.file, 'w')
 	for t in todos:
 		tf.write( f"{t}\n" )
 	tf.close()
@@ -161,23 +183,27 @@ def add(*tasks):
 	return True
 
 
-# Initial checks
-if not isfile(TODO_FILE):
-	if not isdir(dirname(TODO_FILE)): makedirs(dirname(TODO_FILE))
-	open(TODO_FILE,  'a').close() # use 'a' as precaution
-
 # Parse arguments
 parser = ArgumentParser(description = "Organize your todos")
 parser.add_argument('action', choices = ['add', 'list', 'correct'], help = "Select what you want to do")
 parser.add_argument('-t', '--task', action = 'append', dest = 'tasks', help = "The input tasks")
+parser.add_argument('-f', '--file', default = TODO_FILE, help = "Todo.txt file to use")
+tasks_filters = parser.add_argument_group("Tasks filters")
+tasks_filters.add_argument('--tag', dest = 'tags', action = 'append', help = "Filter only specific tags (not case sensitive)")
 args = parser.parse_args()
+
+# Initial checks
+if not isfile(args.file):
+	if not isdir(dirname(args.file)): makedirs(dirname(args.file))
+	open(args.file,  'a').close() # use 'a' as precaution
+print( f"Selected todo.txt file: {args.file}" )
 
 # Program start
 try:
 	if args.action == 'add':
 		add(*args.tasks)
 	elif args.action == 'list':
-		list()
+		list(tags = args.tags)
 except Exception:
 	print(color(traceback.format_exc(), 'red'))
 	exit(1)
